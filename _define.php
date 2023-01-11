65d4a70527cc3da249fefeabda6a24ccb92e7d0a
Pierre <brol31@gmail.com>
2023-01-11 14:51:11 +0100

    oups

--- _define.php
+++ _define.php
@@ -17,7 +17,7 @@ $this->registerModule(
     'Blue Air Message',
     'Deux largeurs et un menu',
     'David Yim, Pierre Van Glabeke',
-    '2.8',
+    '2.8.1',
     [
         'requires' => [['core', '2.24']],
         'type'     => 'theme',
